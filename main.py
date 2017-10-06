7a2bba2751dce4e9693ff50ffeadcab64153e5c3
Syed Himaayath <syedhimaayath155@gmail.com>
2017-10-07 01:46:18 +0530

    module add-a-friend complete

--- main.py
+++ main.py
@@ -8,6 +8,32 @@ existing = raw_input(prompt)
 
 statusMessages = ['Available','Gone hunting','Gadgets matter']
 
+friend_name=[]
+friend_age=[]
+friend_rating=[]
+friend_Is_online=[]
+
+def addFriend():
+    new_friend_name=raw_input("Enter your Friend's name.. ")
+    new_friend_salutation=raw_input('are they Mr. or Ms.? ')
+    new_friend_age=input('Age? ')
+    new_friend_rating=input('Rating? ')
+    if new_friend_salutation =='Mr' or spySalutation=='mr':
+        new_friend_salutation = 'Mr.'
+    else:
+        new_friend_salutation = 'Ms.'
+
+    new_friend_name = new_friend_salutation + new_friend_name
+
+    if new_friend_name > 0 and new_friend_age > 12 and new_friend_age < 50:
+        friend_name.append(new_friend_name)
+        friend_age.append(new_friend_age)
+        friend_rating.append(new_friend_rating)
+    else:
+        print 'The crediantials don\'t match the profile of a spy'
+
+    return len(friend_name)
+
 def addStatus(currentStatusMsg):
     updatedStatusMsg = None
 
@@ -56,18 +82,22 @@ def chat_init(spyName,spySalutation,spyAge,spyRating):
         print "Authentication complete. Welcome %s\nAge : %d\nRating : %.2f"% (spyName,spyAge,spyRating)
         showMenu = 'true'
         currentStatusMsg =None
+
         while showMenu:
-            choices = "What do you want to do? \n1. Add a status update \n2. Close Application"
+            choices = "What do you want to do? \n1. Add a status update \n2. Add a Friend \n3. Close Application"
             selected = input(choices)
 
             if selected == 1:
                 currentStatusMsg = addStatus(currentStatusMsg)
             elif selected == 2:
+                num_of_friends = addFriend()
+                print 'you have %d friends'%(num_of_friends)
+            elif selected == 3:
                 showMenu = False
     else:
         print "Sorry, Age is an issue!!"
 
-if existing=="Y":
+if existing.upper()=="Y":
     chat_init(spyName,spySalutation,spyAge,spyRating)
 else:
     spyName=''
@@ -75,7 +105,7 @@ else:
     spyAge=''
     spyRating=''
     spyName = raw_input("Enter your spy name ")
-    if len(spyName) > 0:
+    if len(spyName) > 0 and spyName.isalpha():
         spySalutation=raw_input("would you like to be called Mister or Miss? ")
         if spySalutation=='Mister' or spySalutation=='mister':
             spySalutation='Mr.'
